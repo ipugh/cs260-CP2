2a822fb1e0f249000c54593617225c6ace0f4a1d
Isaac Pugh <me@isaacpugh.com>
2018-02-15 23:39:57 -0700

    finished!

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
 		e.preventDefault();
         var result = "";
         var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
-		$("#searchSubmit").hide(); 
-        var queryString = $.param({
+		$("#searchSubmit").hide();
+
+		var randomWords = ["rabbit", "china", "man"] 
+		randomWords += ["cliff", "ocean", "tree"];
+		var w = Math.floor(Math.random() * randomWords.length);
+        console.log(randomWords[w]);
+		var queryString = $.param({
             apikey: "7393e0d0-11c9-11e8-b6eb-2feb8ab9bc5e",
-            title: "rabbit",
+            title: randomWords[w],
             classification: "Paintings"
         });
 
@@ -14,14 +19,14 @@ $(document).ready(function() {
             console.log("harvard stuff");
             console.log(data); 
 			var r = Math.floor(Math.random() * data.records.length);
-			if (data.info.totalrecords > 0){
+			if (data.info.totalrecords > 0 && data.records[r].primaryimageurl != null){
 				result += "<img src=" + data.records[r].primaryimageurl + "></img>"; // put in image
 				result += "<h1>" + data.records[r].title + "</h1>";
                 if (data.records[r].period != null) {
 					result += "<h2>" + data.records[r].period + "</h2>";
 				}
 			}
-            else { console.log("should repeat here"); }
+            else { console.log("trying again..."); $("#picture").trigger("click"); }
 			$("#picture").html(result);
             
         });
